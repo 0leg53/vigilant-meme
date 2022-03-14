c16e0594bf733a5b6be346cdd6fda8abc0849b2a
Oleg Sergeev <osergeev@provectus.com>
2022-03-14 10:28:13 +0300

    history log template updated

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -8,13 +8,21 @@ export default {
     state.itemsToAdd = state.itemsToAdd.filter((item) => item.id !== id);
     state.itemsToRemove.push(item);
 
-    state.historyOfAdds.push(`Item named "${item.title}" was added`);
+    state.historyOfAdds.push(
+      `Item named "${item.title}" was added. Id: ${
+        item.id
+      }, time: ${new Date()}`
+    );
   },
   removeItem(state, id) {
     const item = state.itemsToRemove.find((item) => item.id === id);
     state.itemsToRemove = state.itemsToRemove.filter((item) => item.id !== id);
     state.itemsToAdd.push(item);
 
-    state.historyOfRemoves.push(`Item named "${item.title}" was removed`);
+    state.historyOfRemoves.push(
+      `Item named "${item.title}" was removed. Id: ${
+        item.id
+      }, time: ${new Date()}`
+    );
   },
 };
